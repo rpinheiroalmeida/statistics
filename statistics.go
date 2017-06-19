205dbc895a319d2a03b4cddaa05d82bcb7e073d8
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2017-06-18 21:44:36 -0300

    Initial commit

---
+++ statistics.go
@@ -0,0 +1,89 @@
+package statistics
+
+import (
+	"math"
+	"sort"
+
+	"github.com/rpinheiroalmeida/linalg/vector"
+)
+
+func validate(sample Sample) {
+	if len(sample) == 0 {
+		panic("empty sample supplyed")
+	}
+}
+
+type binaryCondition func(v1, v2 float64) float64
+type Sample vector.Vector
+
+func matchingValue(fn binaryCondition, initial float64, sample Sample) float64 {
+	validate(sample)
+
+	current := initial
+	for _, value := range sample {
+		current = fn(current, value)
+	}
+	return current
+}
+
+func (sample Sample) Max() float64 {
+	return matchingValue(math.Max, math.Inf(-1), sample)
+}
+
+func (sample Sample) Min() float64 {
+	return matchingValue(math.Min, math.Inf(+1), sample)
+}
+
+func (sample Sample) Sum() float64 {
+	total := 0.0
+	for _, value := range sample {
+		total += value
+	}
+	return total
+}
+
+func (sample Sample) Mean() float64 {
+	sample.check()
+
+	return sample.Sum() / float64(sample.size())
+}
+
+func (sample Sample) Median() float64 {
+	sample.check()
+
+	sort.Float64s(sample)
+
+	half := sample.size() / 2
+
+	if sample.oddSize() {
+		return sample[half]
+	}
+
+	return Sample{sample[half-1], sample[half]}.Mean()
+}
+
+func (sample Sample) Quantile(percentile float64) float64 {
+	pIndex := int(percentile * float64(sample.size()))
+
+	sort.Float64s(sample)
+
+	return sample[pIndex]
+}
+
+func (sample Sample) size() int {
+	return len(sample)
+}
+
+func (sample Sample) empty() bool {
+	return sample.size() == 0
+}
+
+func (sample Sample) oddSize() bool {
+	return sample.size()%2 == 1
+}
+
+func (sample Sample) check() {
+	if sample.empty() {
+		panic("Operation Not allowed with empty sample")
+	}
+}
---
+++ statistics_test.go
@@ -0,0 +1,212 @@
+package statistics
+
+import (
+	"testing"
+)
+
+type SampleTestTable struct {
+	sample Sample
+	wanted float64
+}
+
+func TestMax(t *testing.T) {
+	cases := []SampleTestTable{
+		{
+			Sample{11.0},
+			11.0,
+		},
+		{
+			Sample{11.0, 12.0},
+			12.0,
+		}, {
+			Sample{11.0, 13.0, 12.0},
+			13.0,
+		},
+	}
+	for _, c := range cases {
+		gotMax := c.sample.Max()
+
+		if gotMax != c.wanted {
+			t.Errorf("Expected max (%v) in (%v) but got (%v)", c.wanted, c.sample, gotMax)
+		}
+	}
+}
+
+func TestMaxFail(t *testing.T) {
+	defer func() {
+		if recover() == nil {
+			t.Error("Expected panic when empty sample")
+		}
+	}()
+
+	Sample{}.Max()
+}
+
+func TestMin(t *testing.T) {
+	cases := []SampleTestTable{
+		{
+			Sample{13.0},
+			13.0,
+		},
+		{
+			Sample{12.0, 13.0},
+			12.0,
+		},
+		{
+			Sample{12.0, 11.0, 13.0},
+			11.0,
+		},
+	}
+	for _, c := range cases {
+		gotMin := c.sample.Min()
+		if gotMin != c.wanted {
+			t.Errorf("Expected max (%v) in (%v) but got (%v)", c.wanted, c.sample, gotMin)
+		}
+	}
+}
+
+func TestMinFail(t *testing.T) {
+	defer func() {
+		if recover() == nil {
+			t.Error("Expected panic when empty sample")
+		}
+	}()
+
+	Sample{}.Min()
+}
+
+func TestSum(t *testing.T) {
+	cases := []SampleTestTable{
+		{
+			Sample{7.0},
+			7.0,
+		},
+		{
+			Sample{32.0, 7.0},
+			39.0,
+		},
+		{
+			Sample{},
+			0.0,
+		},
+	}
+	for _, c := range cases {
+		gotSum := c.sample.Sum()
+		if gotSum != c.wanted {
+			t.Errorf("Expected total (%v) summing up (%v) but got (%v)", c.wanted, c.sample, gotSum)
+		}
+	}
+}
+
+func TestMean(t *testing.T) {
+	cases := []SampleTestTable{
+		{
+			Sample{7.0},
+			7.0,
+		},
+		{
+			Sample{13.0, 14.0},
+			13.5,
+		},
+	}
+	for _, c := range cases {
+		gotMean := c.sample.Mean()
+
+		if gotMean != c.wanted {
+			t.Errorf("Expected mean of (%v) for (%v) but got (%v)", c.wanted, c.sample, gotMean)
+		}
+	}
+}
+
+func TestMeanPanicsWhenEmptySlice(t *testing.T) {
+	defer func() {
+		if recover() == nil {
+			t.Errorf("Expected mean panic when empty sample")
+		}
+	}()
+
+	Sample{}.Mean()
+}
+
+func TestMedian(t *testing.T) {
+	cases := []SampleTestTable{
+		{
+			Sample{7.0},
+			7.0,
+		},
+		{
+			Sample{8.0, 11.0},
+			9.5,
+		},
+		{
+			Sample{7.0, 8.0, 11.0},
+			8.0,
+		},
+		{
+			Sample{7.0, 9.0, 10.0, 17.0},
+			9.5,
+		},
+		{
+			Sample{7.0, 10.0, 17.0, 9.0},
+			9.5,
+		},
+	}
+	for _, c := range cases {
+		gotMedian := c.sample.Median()
+
+		if gotMedian != c.wanted {
+			t.Errorf("Expected median (%v) for (%v) but got (%v)", c.wanted, c.sample, gotMedian)
+		}
+	}
+}
+
+func TestMedianPanicsWhenEmptySlice(t *testing.T) {
+	defer func() {
+		if recover() == nil {
+			t.Errorf("Expected median panic when empty sample")
+		}
+	}()
+
+	Sample{}.Median()
+}
+
+type SampleQuantileTestTable struct {
+	SampleTestTable
+	percentile float64
+}
+
+func TestQuantile(t *testing.T) {
+	cases := []SampleQuantileTestTable{
+		{
+
+			SampleTestTable: SampleTestTable{
+				Sample{7.0},
+				7.0,
+			},
+			percentile: 0.99,
+		},
+		{
+
+			SampleTestTable: SampleTestTable{
+				Sample{7.0, 9.0, 10.0, 13.0, 17.0},
+				13.0,
+			},
+			percentile: 0.75,
+		},
+		{
+
+			SampleTestTable: SampleTestTable{
+				Sample{7.0, 9.0, 13.0, 10.0, 17.0},
+				13.0,
+			},
+			percentile: 0.75,
+		},
+	}
+
+	for _, c := range cases {
+		gotQuantile := c.sample.Quantile(c.percentile)
+		if gotQuantile != c.wanted {
+			t.Errorf("The expected quantile for (%v) with percentile of (%.2f) was (%.2f) but got (%.2f)", c.sample, c.percentile, c.wanted, gotQuantile)
+		}
+	}
+}
